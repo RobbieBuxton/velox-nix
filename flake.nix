8b69883bd3afdbe8be86619d6714bf9c7aebff2a
Robbie Buxton <robbiesbuxton@gmail.com>
2024-07-29 04:28:45 +0100

    added init flake
    Signed-off-by: Robbie Buxton <robbiesbuxton@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Velox a C++ database acceleration library";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system} = pkgs.callPackage ./package.nix {};
+    };
+}
